046d8ab860df89d5bdd5afbc08a3596cb6e49785
Michael Allen <mickalleny2k@yahoo.co.uk>
2024-05-12 15:04:25 +0100

    real server

---
+++ projectDAO.py
@@ -0,0 +1,66 @@
+import mysql.connector
+import dbconfig as cfg
+class ProjectDAO:
+    connection=""
+    cursor =''
+    host=       ''
+    user=       ''
+    password=   ''
+    database=   ''
+    
+    def __init__(self):
+        self.host=       cfg.mysql['host']
+        self.user=       cfg.mysql['user']
+        self.password=   cfg.mysql['password']
+        self.database=   cfg.mysql['database']
+
+    def getcursor(self): 
+        self.connection = mysql.connector.connect(
+            host=       self.host,
+            user=       self.user,
+            password=   self.password,
+            database=   self.database,
+        )
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def closeAll(self):
+        self.connection.close()
+        self.cursor.close()
+         
+    def getAll(self):
+        cursor = self.getcursor()
+        sql="select * from project"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        #print(results)
+        for result in results:
+            #print(result)
+            returnArray.append(self.convertToDictionary(result))
+        
+        self.closeAll()
+        return returnArray
+
+    def findByID(self, id):
+        cursor = self.getcursor()
+        sql="select * from project where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        returnvalue = self.convertToDictionary(result)
+        self.closeAll()
+        return returnvalue
+
+    def convertToDictionary(self, resultLine):
+        attkeys=['id','name','staff']
+        project = {}
+        currentkey = 0
+        for attrib in resultLine:
+            project[attkeys[currentkey]] = attrib
+            currentkey = currentkey + 1 
+        return project
+
+        
+projectDAO = ProjectDAO()
--- server.py
+++ server.py
@@ -1,15 +1,81 @@
-from flask import Flask
+# flask server that links to a DAO
+# author: Michael Allen
+
+from flask import Flask, request, jsonify, abort
 #from projectDAOskeleton import projectDAO
 #import projectDAO
-#from projectDAO import projectDAO
+from projectDAO import projectDAO
+#import pyautogui
 #import deleteRow
 
-#app = Flask(__name__, static_url_path='', static_folder='.')
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='.')
+#app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return "Web Services and Applications Project"
+        return "Hello World ! Web Services and Applications Project"
+
+# find by id
+# curl http://127.0.0.1:5000/project/1
+
+@app.route('/project/<int:id>', methods=['GET'])
+def findbyid(id):
+        return jsonify(projectDAO.findByID(id))
+
+# getall
+# curl http://127.0.0.1:5000/project
+
+@app.route('/project', methods=['GET'])
+def getall():
+        return jsonify(projectDAO.getAll())
+
+#curl  -i -H "Content-Type:application/json" -X POST -d "{\"id\":5,\"name\":\"High Support Needs\",\"staff\":12}" http://127.0.0.1:5000/project
+@app.route('/project', methods=['POST'])
+def create():
+    
+    if not request.json:
+        abort(400)
+    # other checking 
+    project = {
+        "id": request.json['id'],
+        "name": request.json['name'],
+        "staff": request.json['staff'],
+    }
+    addedproject = projectDAO.create(project)
+    #pyautogui.hotkey('f5') #Simulates F5 key press = page refresh
+    return jsonify(addedproject)
+
+#curl  -i -H "Content-Type:application/json" -X PUT -d "{\"id\":5,\"name\":\"High Support Needs\",\"staff\":111}" http://127.0.0.1:5000/project/5
+@app.route('/project/<int:id>', methods=['PUT'])
+def update(id):
+    foundProject = projectDAO.findByID(id)
+    print(foundProject)
+    if not foundProject:
+        abort(404)
+    
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    print(reqJson)
+    #if 'id' in reqJson and type(reqJson['id']) is not int:
+        #print("id is not an integer")
+        #abort(400)
+    if 'id' in reqJson:
+        foundProject['id'] = reqJson['id']
+    if 'name' in reqJson:
+        foundProject['name'] = reqJson['name']
+    if 'staff' in reqJson:
+        foundProject['staff'] = reqJson['staff']
+    projectDAO.update(id,foundProject)
+    #pyautogui.hotkey('f5') #Simulates F5 key press = page refresh
+    return jsonify(foundProject)    
+
+@app.route('/project/<int:id>' , methods=['DELETE'])
+def delete(id):
+    projectDAO.delete(id)
+    return jsonify({"done":True})
+    #print("Delete done. Row " +id+ "was deleted successfully.")
+    #return jsonify(projectDAO.delete(id))
 
 if __name__ == "__main__":
     app.run(debug = True)
